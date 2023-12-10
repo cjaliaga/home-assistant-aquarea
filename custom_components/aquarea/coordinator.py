8d0a47ff871e4597ad26465c8f73ecb5c3a6d43d
Carlos J. Aliaga <carlos@cjaliaga.net>
2023-12-10 20:43:45 +0100

    Energy consumption sensors (#64)
    * Accumulated Energy Consumption Sensors
    
    * Added energy sensors that reset cycle
    
    * Adding translations to sensors
    
    * Checking if any of the zone support cooling to enable the sensor

--- custom_components/aquarea/coordinator.py
+++ custom_components/aquarea/coordinator.py
@@ -11,11 +11,14 @@ from homeassistant.const import CONF_USERNAME
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryAuthFailed
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+from homeassistant.util import dt
 
 from .const import DOMAIN
 
 DEFAULT_SCAN_INTERVAL_SECONDS = 10
 SCAN_INTERVAL = timedelta(seconds=DEFAULT_SCAN_INTERVAL_SECONDS)
+CONSUMPTION_REFRESH_INTERVAL_MINUTES = 1
+CONSUMPTION_REFRESH_INTERVAL = timedelta(minutes=CONSUMPTION_REFRESH_INTERVAL_MINUTES)
 _LOGGER = logging.getLogger(__name__)
 
 
@@ -51,11 +54,14 @@ class AquareaDataUpdateCoordinator(DataUpdateCoordinator):
         return self._device
 
     async def _async_update_data(self) -> None:
-        """Fetch data from Aquarea Smart Cloud Service"""
+        """Fetch data from Aquarea Smart Cloud Service."""
         try:
+            # We are not getting consumption data on the first refresh, we'll get it on the next ones
             if not self._device:
                 self._device = await self._client.get_device(
-                    device_info=self._device_info
+                    device_info=self._device_info,
+                    consumption_refresh_interval=CONSUMPTION_REFRESH_INTERVAL,
+                    timezone=dt.DEFAULT_TIME_ZONE,
                 )
             else:
                 await self.device.refresh_data()
